422fb627c22802e9fe49e6f6f80bd1963ad232b2
Mahziar MirAzimi <seyedmahziar59@gmail.com>
2022-11-18 14:45:16 +0330

    Without recursive function, we can use this for the files which are in current directory, not nested folders!

--- Del_Mem.py
+++ Del_Mem.py
@@ -7,4 +7,9 @@ import os
 def get_dir_size(path='.'):
     # size of path in KB
     total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size // 1024
+    return total
 
